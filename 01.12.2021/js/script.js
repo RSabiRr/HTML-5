e6a05d278fc09629df19319579e5ef00fbc7eed5
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-12-01 23:54:00 +0400

    Add files via upload

---
+++ 01.12.2021/js/script.js
@@ -0,0 +1,72 @@
+
+//  var count=0;
+
+// for (let i = 1; i <1000; i++) {
+   
+//  if (i%3==0 && i%5==0 ) {
+//      count+=i;
+//  } 
+        
+// }
+
+// console.log(count);
+
+// var area;
+
+// let a=Number(prompt("Enter number 1"));
+// let b=Number(prompt("Enter number 2"));
+
+
+
+// area=a*b;
+
+// console.log(area);
+
+////////////////////////////////////////////////////////////////////
+
+
+// var count;
+
+// let a=Number(prompt("Enter number 1"));
+// let b=Number(prompt("Enter number 2"));
+
+// count=a+b;
+
+// if (a==b) {
+//     console.log(count*3);
+// }else{
+//     console.log(count);
+// }
+
+///////////////////////////////////////////////////////////////////
+
+// let a=Number(prompt("Enter number 1"));
+// let b=Number(prompt("Enter number 2"));
+
+// if (a<0 || b<0) {
+//     console.log("2 ededde menfidir");
+// }else if(a>0 || b>0) {
+//     console.log("2 ededde musbetdir");
+// }
+
+
+// let a=Number(prompt("Sirani daxil edin:"));
+//  let b="";
+// for (let i = 0; i < a; i++) {
+    
+//     for (let j = 0; j <= i; j++) {
+        
+//         b+="*";
+    
+//     }
+//     console.log(b);
+//     b=" ";
+
+// }
+        
+ 
+
+
+
+
+
